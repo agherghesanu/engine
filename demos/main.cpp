cc6bd3e2a9a8670abdd0e28782eb14ab97203434
Alexandru Ghergheşanu <M.Gherghesanu@student.tudelft.nl>
2026-02-04 03:16:38 +0200

    add vecotr implmentaion and testing added construcor, basic functions and operator overload

--- demos/main.cpp
+++ demos/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-using namespace std;
+#include <cyclone/core.h>
+
+
 
 int main() {
-	cout << "This is the main demo application." << endl;
-	return 0;
+	cyclone::Vector3 pos(1.0, 2.0, 3.0);
+	cyclone::Vector3 velocity(4.0, 5.0, 6.0);
+	std::cout << "Initial Position: (" << pos.x << ", " << pos.y << ", " << pos.z << ")\n";
+
+	pos += velocity;
+
+	std::cout << "Updated Position: (" << pos.x << ", " << pos.y << ", " << pos.z << ")\n";
+
+	velocity *= 0.5;
+
+	std::cout << "Scaled Velocity: (" << velocity.x << ", " << velocity.y << ", " << velocity.z << ")\n";
+
+	cyclone::Vector3 v(3.0, 4.0, 0.0);
+	std::cout << "Vector v Magnitude: " << v.magnitude() << "\n";
+
+	v.normalize();
+
+	std::cout << "Normalized Vector v: (" << v.x << ", " << v.y << ", " << v.z << ")\n";
 }
